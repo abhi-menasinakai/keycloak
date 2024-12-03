5577e6a99b74d835ce05eebeea151dfb9b4a010e
abhi-menasinakai <166580338+abhi-menasinakai@users.noreply.github.com>
2024-12-03 09:48:42 +0530

    Update server.sh

--- keycloak/entryscripts/server.sh
+++ keycloak/entryscripts/server.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 set -e
-
 echo "Running custom setup tasks..."
 # Custom setup commands can go here
-
-echo "Running build to apply configuration changes..."
+#echo "Running build to apply configuration changes..."
 /opt/keycloak/bin/kc.sh build
+echo "Starting Keycloak server with verbose logging..."
 
-echo "Starting Keycloak server..."
-exec /opt/keycloak/bin/kc.sh start-dev
-
+# Start Keycloak with verbose logging options and output logs to CLI
+exec /opt/keycloak/bin/kc.sh start \
+    --hostname auth.konectar.io \
+    --spi-login-protocol-openid-connect-legacy-logout-redirect-uri=true
+    --optimized
